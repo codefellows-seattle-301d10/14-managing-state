b6b0adeb4fa812992b2be03b5f0d4af14f478f9a
Lee Broxson <broxsonl@gmail.com>
2016-09-01 15:44:13 -0700

    Finished next COMMENT for articleController.loadByAuthor.

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -32,6 +32,17 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /* loadByAuthor first runs the Article.findWhere function to get our data.
+   We're pulling everything from the sql database of articles WHERE id = the id
+  of the context (current article) that is being passed in. Then we run
+  articleData, which is defined above. articleData attaches the value of the
+  Context.article to the value of the placeholder article. Then we run the
+  next() function, which is articleController.index, as described by the
+   routes.js file.
+
+   This function's execution path is that it runs when
+   page('/author/:authorName' runs.
+  */
   articleController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
