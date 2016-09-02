d4cef1bc7e84cccc1cf9d47f8f5d947800b3371d
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-09-02 08:23:42 -0700

    commenting for loadbyAuthor function

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -11,7 +11,7 @@
     }
   };
 
-  // This method also runs callbacks via next() in order to persist state from one use to the next. State is allowed to be persisted via the context object. This method loads a specific article when read more is clicked. The path of that article is /articles/id.  It finds the ID, puts it into the context object's params property, and users articleData to render the article. 
+  // This method also runs callbacks via next() in order to persist state from one use to the next. State is allowed to be persisted via the context object. This method loads a specific article when read more is clicked. The path of that article is /articles/id.  It finds the ID, puts it into the context object's params property, and users articleData to render the article. Then, the next() function is called to move to the next function.
 
   articleController.loadById = function(ctx, next) {
     var articleData = function(article) {
@@ -21,7 +21,7 @@
     Article.findWhere('id', ctx.params.id, articleData);
   };
 
-  // COMMENT: What does this method do?  What is it's execution path?
+  // This method is the same as loadById, except it loads specific articles by the author property of the context object, rather than by id. The findWhere function finds the authorName property on the context object, and replaces any whitespace with a + sign, such that the URL path will be /author/firstname+lastname. 
   articleController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
